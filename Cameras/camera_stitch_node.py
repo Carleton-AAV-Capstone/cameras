97d39e44052aa3b4a759b1462ed13009c2fb0919
Ryan Dash <ryandash@cmail.carleton.ca>
2024-11-26 13:59:16 -0500

    Add camera calibration Remove distortion and crop camera

--- Cameras/camera_stitch_node.py
+++ Cameras/camera_stitch_node.py
@@ -3,20 +3,29 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
+import numpy as np
 import sys
 import threading
 from rclpy.qos import QoSProfile
 
+# Load the camera matrix and distortion coefficients from calibration files
+def load_calibration_data():
+    mtx = np.load('camera_matrix.npy')
+    dist = np.load('distortion_coefficients.npy')
+    return mtx, dist
+
 class VideoCaptureThread(threading.Thread):
-    def __init__(self, device, topic_name, node):
+    def __init__(self, device, topic_name, node, mtx, dist):
         super().__init__()
         self.device = device
         self.node = node
         self.bridge = CvBridge()
-        self.publisher = node.create_publisher(Image, topic_name, QoSProfile(depth=100))
+        self.publisher = node.create_publisher(Image, topic_name, QoSProfile(depth=10))
+        self.mtx = mtx
+        self.dist = dist
+
         self.capture = cv2.VideoCapture(self.device)
 
-        # Set camera properties (ensure this matches desired frame rate/resolution)
         self.capture.set(cv2.CAP_PROP_FPS, 60)
         self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
         self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
@@ -32,7 +41,9 @@ class VideoCaptureThread(threading.Thread):
             ret, frame = self.capture.read()
             if ret:
                 try:
-                    image_msg = self.bridge.cv2_to_imgmsg(frame, encoding='bgr8')
+                    undistorted_frame = cv2.undistort(frame, self.mtx, self.dist)
+
+                    image_msg = self.bridge.cv2_to_imgmsg(undistorted_frame, encoding='bgr8')
                     self.publisher.publish(image_msg)
                 except Exception as e:
                     self.node.get_logger().error(f"Failed to publish frame: {str(e)}")
@@ -44,14 +55,14 @@ class VideoCaptureThread(threading.Thread):
         self.capture.release()
 
 class VideoFeedsNode(Node):
-    def __init__(self, video_devices):
+    def __init__(self, video_devices, mtx, dist):
         super().__init__('video_feeds_node')
         self.video_devices = video_devices
         self.capture_threads = []
 
         for index, device in enumerate(self.video_devices):
             topic_name = f'/camera/device_{index}/image_raw'  # Each camera has its own topic
-            capture_thread = VideoCaptureThread(device, topic_name, self)
+            capture_thread = VideoCaptureThread(device, topic_name, self, mtx, dist)
             capture_thread.start()
             self.capture_threads.append(capture_thread)
 
@@ -64,10 +75,12 @@ class VideoFeedsNode(Node):
 def main(args=None):
     rclpy.init(args=args)
 
-    # Initialize video devices (replace with actual device paths)
-    video_devices = ["/dev/video2", "/dev/video4"]
+    mtx, dist = load_calibration_data()
 
-    node = VideoFeedsNode(video_devices)
+    # Initialize video devices (replace with actual device paths or pass in as arguments)
+    video_devices = ["/dev/video0"]
+
+    node = VideoFeedsNode(video_devices, mtx, dist)
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
--- README.md
+++ README.md
@@ -5,7 +5,29 @@ Code to publish camera feed to ROS2
 If using pycharm in linux
 settings -> Project -> interpreter -> select interpreter -> show all -> show interpreter paths
 add the following:
+```
 /opt/ros/humble/lib/python3.10/site-packages
 /opt/ros/humble/local/lib/python3.10/dist-packages
+```
 
-This will let ros2 packages be understood by the python in pycharm
+This will let ros2 packages be understood by python in pycharm
+
+linux ~/.bashrc changes
+```bash
+echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+echo "source ~/ros2_ws/install/setup.bash" >> ~/.bashrc
+echo "export PATH=/usr/local/cuda-11.5/bin:\$PATH" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=/usr/local/cuda-11.5/lib64:\$LD_LIBRARY_PATH" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:\$LD_LIBRARY_PATH" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=/opt/ros/humble/lib:\$LD_LIBRARY_PATH" >> ~/.bashrc
+echo "export WORKON_HOME=\$HOME/.virtualenvs" >> ~/.bashrc
+echo "export PROJECT_HOME=\$HOME/Devel" >> ~/.bashrc
+echo "export PYTHONPATH=/opt/ros/humble/lib/python3.10/site-packages:\$PYTHONPATH" >> ~/.bashrc
+echo "export AMENT_PREFIX_PATH=/opt/ros/humble:\$AMENT_PREFIX_PATH" >> ~/.bashrc
+echo "export ROS_DOMAIN_ID=0" >> ~/.bashrc
+echo "export ROS_DISTRO=humble" >> ~/.bashrc
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
+
+source ~/.bashrc
+```
+
